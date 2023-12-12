635c32a70b73e01753311aaa1b19c7e9a7f1141a
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-12 22:06:00 +0500

    Exported components

---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const navItem = [
+    {
+        id: 1,
+        href: "/",
+        title: "Home"
+    },
+    {
+        id: 2,
+        href: "about",
+        title: "About"
+    },
+    {
+        id: 3,
+        href: "contact",
+        title: "Contact"
+    },
+];
